bb78fbda258a6fca978096c5c263132edc763e0d
Artem Koton <kotonartem@protonmail.com>
2023-11-01 16:57:30 +0300

    Add CMAKE_TOOLCHAIN_FILE environment varaible

--- test/build-and-run.sh
+++ test/build-and-run.sh
@@ -12,9 +12,8 @@ BUILD_DIR=cmake-build
 mkdir "${BUILD_DIR}"
 
 cmake \
-  -DCMAKE_TOOLCHAIN_FILE=/opt/vcpkg/scripts/buildsystems/vcpkg.cmake \
   -DCMAKE_BUILD_TYPE=RelWithDebInfo \
-   -S . -B "${BUILD_DIR}"
+  -S . -B "${BUILD_DIR}"
 
 cmake --build "${BUILD_DIR}"
 
