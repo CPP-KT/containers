6c31c8f5a9b1dbddea0402c0448d974e87658361
Artem Koton <kotonartem@protonmail.com>
2023-12-12 12:16:54 +0300

    Update CMake to 3.28.0

--- common/install-cmake.sh
+++ common/install-cmake.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -euo pipefail
 
-CMAKE_VERSION=3.27.5
+CMAKE_VERSION=3.28.0
 CMAKE_ARCH=linux-$(uname -m)
 
 DOWNLOAD_URL="https://github.com/Kitware/CMake/releases/download/v${CMAKE_VERSION}/"
