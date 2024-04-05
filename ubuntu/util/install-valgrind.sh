ba394cff57a2c6f699703ff1060e1966e54eadd8
Artem Koton <kotonartem@protonmail.com>
2024-04-05 05:39:28 +0300

    Update Valgrind to 3.22.0

--- ubuntu/util/install-valgrind.sh
+++ ubuntu/util/install-valgrind.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -euo pipefail
 
-VALGRIND_VERSION=3.21.0
+VALGRIND_VERSION=3.22.0
 
 DOWNLOAD_URL="https://sourceware.org/pub/valgrind"
 FILENAME="valgrind-${VALGRIND_VERSION}.tar.bz2"
