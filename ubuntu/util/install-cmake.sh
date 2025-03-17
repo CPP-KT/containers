7d9b7ae7edf7ca0379248316cc869498f74825c3
Artem Koton <kotonartem@protonmail.com>
2025-03-17 17:50:09 +0300

    Update CMake to 3.31.6

--- ubuntu/util/install-cmake.sh
+++ ubuntu/util/install-cmake.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -euo pipefail
 
-CMAKE_VERSION=3.30.5
+CMAKE_VERSION=3.31.6
 CMAKE_ARCH=linux-$(uname -m)
 
 DOWNLOAD_URL="https://github.com/Kitware/CMake/releases/download/v${CMAKE_VERSION}/"
