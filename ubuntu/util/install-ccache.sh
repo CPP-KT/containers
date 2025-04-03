eccda3e017e6418d702331e3f6da3220d292ede0
ivan medvedev <vmedvedev1017@gmail.com>
2025-04-04 01:56:43 +0300

    Add ccache persistent warmup based on template task

--- ubuntu/util/install-ccache.sh
+++ ubuntu/util/install-ccache.sh
@@ -7,3 +7,39 @@ apt-get install -y --no-install-recommends ccache
 mkdir "${CCACHE_DIR}"
 chmod -R a+rwx "${CCACHE_DIR}"
 
+
+### WARM UP CCACHE ###
+
+REPO_URL="https://github.com/cpp-kt/template-task.git"
+CLONE_DIR="/opt/template-task"
+
+export CCACHE_NOHASHDIR=1
+export CCACHE_BASEDIR=${CLONE_DIR}
+export CCACHE_IGNOREOPTIONS=-ffile-prefix-map*
+
+if [ ! -d "${CLONE_DIR}" ]; then
+    git clone "${REPO_URL}" "${CLONE_DIR}"
+fi
+
+pushd "${CLONE_DIR}"
+export CCACHE_LOGFILE=ccache.log
+
+if [ -f CMakePresets.json ]; then
+    PRESETS=$(jq -r '.version as $v | .configurePresets[] | select(.hidden != true) | .name' CMakePresets.json)
+else
+    echo "CMakePresets.json not found!"
+    exit 1
+fi
+
+for PRESET in $PRESETS; do
+    echo "Building with: ${PRESET}"
+    
+    cmake --preset  ${PRESET}
+    cmake --build build/${PRESET}
+done
+
+echo "All presets are built."
+
+popd
+
+rm -rf ${CLONE_DIR}
